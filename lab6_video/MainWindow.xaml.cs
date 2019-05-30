af6f646087a764ffeb6e326a5b42569039ca5148
juziem <lus0800@mail.ru>
2019-05-30 13:19:23 +0700

    add smth

--- lab6_video/MainWindow.xaml.cs
+++ lab6_video/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Media;
+using Microsoft.Win32;
+using System.Windows.Controls.Primitives;
 
 namespace lab6_video
 {
@@ -20,9 +23,51 @@ namespace lab6_video
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        MediaPlayer player = new MediaPlayer();
+        System.Windows.Threading.DispatcherTimer Timer;
+        TimeSpan ts;
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Timer = new System.Windows.Threading.DispatcherTimer();
+            Timer.Tick += new EventHandler(dispatcherTimer_Tick);
+            Timer.Interval = new TimeSpan(0, 0, 1);
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            head.Content = player.Position.Hours + ":" + player.Position.Minutes + ":" + player.Position.Seconds;
+            slide.Value = player.Position.TotalSeconds;
+        }
+
+       
+        private void play_Click(object sender, RoutedEventArgs e)
+        {
+            player.Play();
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.ShowDialog();
+            //slide.Source = new Uri(dlg.FileName, UriKind.Relative);
+        }
+
+        private void pause_Click(object sender, RoutedEventArgs e)
+        {
+            player.Pause();
+        }
+
+        private void stop_Click(object sender, RoutedEventArgs e)
+        {
+            player.Stop();
+            Timer.Stop();
+            head.Content = "";
         }
     }
 }
